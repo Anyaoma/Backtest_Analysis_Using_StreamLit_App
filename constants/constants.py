23f8fb27fdbb3cf8749361d12e7f5a09bd44ad60
Anyaoma <51029157+Anyaoma@users.noreply.github.com>
2025-03-22 17:59:35 +0000

    Add files via upload

---
+++ constants/constants.py
@@ -0,0 +1,10 @@
+API_KEY = "5e83fdfb5bc07b2c2b8b545a6469fd5e-7a0c383ce4e2fa52f01856e6e3d0fc28"
+ACCOUNT_ID = "101-004-29366199-001"
+#OANDA_URL = "https://stream-fxpractice.oanda.com/v3"
+OANDA_URL = "https://api-fxpractice.oanda.com/v3"
+SECURE_HEADER = {
+    "Authorization": f"Bearer {API_KEY}",
+     "Content-Type": "application/json"}
+BUY = 1
+SELL = -1
+NONE = 0
