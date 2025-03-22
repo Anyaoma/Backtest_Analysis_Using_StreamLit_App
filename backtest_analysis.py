dca8c971e26d35ddedff383a4dd9aac9aab78646
Anyaoma <51029157+Anyaoma@users.noreply.github.com>
2025-03-22 18:45:02 +0000

    Update backtest_analysis.py

--- backtest_analysis.py
+++ backtest_analysis.py
@@ -7,7 +7,7 @@ import constants.constants as comps
 from technicals.indicators import RSI
 from technicals.patterns import apply_properties_patterns
 from oanda_api.oanda_api import oandaApi as api
-from backtest_analysis.strategy_combined import GuruTester
+from Strategy_setter.strategy_setter import GuruTester
 
 
 SELECT_PAIRS_FROM = ['AUD_JPY','EUR_AUD','EUR_USD','GBP_AUD']
