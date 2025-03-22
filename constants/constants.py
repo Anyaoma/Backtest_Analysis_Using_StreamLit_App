13c94736f7ecd33837b5210fcc51d429b3986e8b
Anyaoma <51029157+Anyaoma@users.noreply.github.com>
2025-03-22 18:15:06 +0000

    Update constants.py

--- constants/constants.py
+++ constants/constants.py
@@ -1,5 +1,7 @@
-API_KEY = "5e83fdfb5bc07b2c2b8b545a6469fd5e-7a0c383ce4e2fa52f01856e6e3d0fc28"
-ACCOUNT_ID = "101-004-29366199-001"
+import os
+
+API_KEY = os.environ.get("API_KEY")
+ACCOUNT_ID = os.environ.get("ACCOUNT_ID")
 #OANDA_URL = "https://stream-fxpractice.oanda.com/v3"
 OANDA_URL = "https://api-fxpractice.oanda.com/v3"
 SECURE_HEADER = {
